a23fcc457e8e0e168a200ba4c0adc5282ce45c3b
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-11-03 21:32:12 +0400

    Mock database is completed

---
+++ DemoApp/DemoApplication/Database/Database.cs
@@ -0,0 +1,19 @@
+﻿using DemoApplication.Controllers;
+using DemoApplication.Database.Models;
+
+namespace DemoApplication.Database
+{
+    public class DatabaseAccess
+    {
+        public static List<Flower> Flowers { get; set; } = new List<Flower>
+        {
+            new Flower("MAJESTY PALM", 259),
+            new Flower("Lotus", 259),
+            new Flower("Rose", 213),
+            new Flower("Jasmine", 234),
+            new Flower("Sunflower", 23),
+            new Flower("Daisy", 23),
+            new Flower("Tulip", 130),
+        };
+    }
+}
---
+++ DemoApp/DemoApplication/Database/Models/Flower.cs
@@ -0,0 +1,14 @@
+﻿namespace DemoApplication.Database.Models
+{
+    public class Flower
+    {
+        public Flower(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
