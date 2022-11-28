df992d2be05c68b8fb54dbe118d739b397e2c96c
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-11-28 17:48:09 +0400

    List of books endpoint completed

--- DemoApp/DemoApplication/Controllers/Admin/BookController.cs
+++ DemoApp/DemoApplication/Controllers/Admin/BookController.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DemoApplication.Database;
+using DemoApplication.ViewModels.Admin.Book;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DemoApplication.Controllers.Admin
 {
     [Route("admin/book")]
     public class BookController : Controller
     {
+        private readonly DataContext _dataContext;
+
+        public BookController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
         [HttpGet("list", Name = "admin-book-list")]
         public IActionResult List()
         {
-            return View("~/Views/Admin/Book/List.cshtml");
+            var model = _dataContext.Books
+                .Select(b => new ListItemViewModel(b.Id, b.Title, b.Price, $"{b.Author.FirstName} {b.Author.LastName}", b.CreatedAt))
+                .ToList();
+
+            return View("~/Views/Admin/Book/List.cshtml", model);
         }
     }
 }
---
+++ DemoApp/DemoApplication/ViewModels/Admin/Book/ListItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace DemoApplication.ViewModels.Admin.Book
+{
+    public class ListItemViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Author { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public ListItemViewModel(int id, string title, decimal price, string author, DateTime createdAt)
+        {
+            Id = id;
+            Title = title;
+            Price = price;
+            Author = author;
+            CreatedAt = createdAt;
+        }
+    }
+}
