6fd244894054d2bba414a5a9384b2ba09124a357
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-12-06 21:50:16 +0400

    Basket management based on pure js completed

---
+++ DemoApp/DemoApplication/wwwroot/client/assets/js/cookie-management.js
@@ -0,0 +1,19 @@
+﻿
+function add_cookie(name, value) {
+    var cookie = [name, '=', JSON.stringify(value)].join('');
+    document.cookie = cookie;
+}
+
+function read_cookie(name) {
+    var result = document.cookie.match(new RegExp(name + '=([^;]+)'));
+    result && (result = JSON.parse(result[1]));
+    return result;
+}
+
+function is_cookie_exists(name) {
+    return document.cookie.indexOf(`${name}=`) == 0;
+}
+
+function reset_cookie(name) {
+    document.cookie = `${name}=[]`
+}
