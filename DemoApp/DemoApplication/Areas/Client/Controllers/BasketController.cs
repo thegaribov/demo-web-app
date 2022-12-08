494c358531f9b5cd52b3283f32c992d5792bec0f
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-12-08 20:50:57 +0400

    Add product to basket action method statements cleaned

--- DemoApp/DemoApplication/Areas/Client/Controllers/BasketController.cs
+++ DemoApp/DemoApplication/Areas/Client/Controllers/BasketController.cs
@@ -3,6 +3,7 @@ using DemoApplication.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json;
+using System.Xml;
 
 namespace DemoApplication.Areas.Client.Controllers
 {
@@ -28,33 +29,46 @@ namespace DemoApplication.Areas.Client.Controllers
             }
 
             var productCookieValue = HttpContext.Request.Cookies["products"];
-            if (productCookieValue is null)
-            {
-                var productsCookieViewModel = new List<ProductCookieViewModel>
-                {
-                   new ProductCookieViewModel(product.Id, product.Title, string.Empty,  1, product.Price, product.Price)
-                };
+            var productsCookieViewModel = productCookieValue is not null 
+                ?  JsonSerializer.Deserialize<List<ProductCookieViewModel>>(productCookieValue)
+                : new List<ProductCookieViewModel> { };
 
-                HttpContext.Response.Cookies.Append("products", JsonSerializer.Serialize(productsCookieViewModel));
+            var productCookieViewModel = productsCookieViewModel!.FirstOrDefault(pcvm => pcvm.Id == id);
+            if (productCookieViewModel is null)
+            {
+                productsCookieViewModel
+                    !.Add(new ProductCookieViewModel(product.Id, product.Title, string.Empty, 1, product.Price, product.Price));
             }
             else
             {
-                var productsCookieViewModel = JsonSerializer.Deserialize<List<ProductCookieViewModel>>(productCookieValue);
-                var productCookieViewModel = productsCookieViewModel!.FirstOrDefault(pcvm => pcvm.Id == id);
-
-                if (productCookieViewModel is null)
-                {
-                    productsCookieViewModel
-                        !.Add(new ProductCookieViewModel(product.Id, product.Title, string.Empty, 1, product.Price, product.Price));
-                }
-                else
-                {
-                    productCookieViewModel.Quantity += 1;
-                    productCookieViewModel.Total = productCookieViewModel.Quantity * productCookieViewModel.Price;
-                }
-
-                HttpContext.Response.Cookies.Append("products", JsonSerializer.Serialize(productsCookieViewModel));
+                productCookieViewModel.Quantity += 1;
+                productCookieViewModel.Total = productCookieViewModel.Quantity * productCookieViewModel.Price;
             }
+
+            HttpContext.Response.Cookies.Append("products", JsonSerializer.Serialize(productsCookieViewModel));
+
+            return RedirectToRoute("client-home-index");
+        }
+
+        [HttpGet("delete/{id}", Name = "client-basket-delete")]
+        public async Task<IActionResult> DeleteProductAsync([FromRoute] int id)
+        {
+            var product = await _dataContext.Books.FirstOrDefaultAsync(b => b.Id == id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            var productCookieValue = HttpContext.Request.Cookies["products"];
+            if (productCookieValue is null)
+            {
+                return NotFound();
+            }
+
+            var productsCookieViewModel = JsonSerializer.Deserialize<List<ProductCookieViewModel>>(productCookieValue);
+            productsCookieViewModel!.RemoveAll(pcvm => pcvm.Id == id);
+
+            HttpContext.Response.Cookies.Append("products", JsonSerializer.Serialize(productsCookieViewModel));
 
             return RedirectToRoute("client-home-index");
         }
