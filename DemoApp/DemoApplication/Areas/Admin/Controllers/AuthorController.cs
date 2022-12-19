44d89bf7d9ef302162349e47c93bbb1ff02f3bff
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-12-19 21:31:15 +0400

    Adding author completed

--- DemoApp/DemoApplication/Areas/Admin/Controllers/AuthorController.cs
+++ DemoApp/DemoApplication/Areas/Admin/Controllers/AuthorController.cs
@@ -1,7 +1,10 @@
-﻿using DemoApplication.Areas.Admin.ViewModels.Author;
+﻿using DemoApplication.Areas.Admin.ViewComponents;
+using DemoApplication.Areas.Admin.ViewModels.Author;
 using DemoApplication.Database;
+using DemoApplication.Database.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace DemoApplication.Areas.Admin.Controllers
 {
@@ -20,10 +23,39 @@ namespace DemoApplication.Areas.Admin.Controllers
         public IActionResult List()
         {
             var model = _dataContext.Authors
+                .OrderByDescending(a => a.CreatedAt)
                 .Select(a => new ListItemViewModel(a.Id, a.FirstName, a.LastName))
                 .ToList();
 
             return View(model);
+        }
+
+        [HttpPost("add", Name = "admin-author-add")]
+        public async Task<IActionResult> AddAsync(AddViewModel? model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var addModelViewComponent = ViewComponent(nameof(AddModal), model);
+                addModelViewComponent.StatusCode = (int)HttpStatusCode.BadRequest;
+                return addModelViewComponent;
+            }
+
+            var author = new Author
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+            };
+            
+            await _dataContext.Authors.AddAsync(author);
+            await _dataContext.SaveChangesAsync();
+
+            var responseModel = new ListItemViewModel(author.Id, author.FirstName, author.LastName);
+
+            var listItemPartialView = PartialView("Partials/Author/_ListItem", responseModel);
+            listItemPartialView.StatusCode = (int)HttpStatusCode.Created;
+            return listItemPartialView;
         }
     }
 }
---
+++ DemoApp/DemoApplication/Areas/Admin/ViewComponents/AddModal.cs
@@ -0,0 +1,15 @@
+﻿using DemoApplication.Areas.Admin.ViewModels.Author;
+using DemoApplication.Areas.Client.ViewModels.Basket;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace DemoApplication.Areas.Admin.ViewComponents
+{
+    public class AddModal : ViewComponent
+    {
+        public IViewComponentResult Invoke(AddViewModel? model = null)
+        {
+            return View(model ?? new AddViewModel());
+        }
+    }
+}
