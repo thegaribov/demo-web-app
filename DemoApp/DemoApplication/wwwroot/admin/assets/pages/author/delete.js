80f5dc92bb7e8ba76aa1dadd13df7910b8a09053
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-12-19 21:47:06 +0400

    Delete js cleaned

--- DemoApp/DemoApplication/wwwroot/admin/assets/pages/author/delete.js
+++ DemoApp/DemoApplication/wwwroot/admin/assets/pages/author/delete.js
@@ -3,20 +3,13 @@
 
     let url = e.target.href;
     let row = e.target.parentElement.parentElement;
-    console.log(url)
-    console.log(row)
-
 
     $.ajax({
         url: url,
         type: "DELETE",
         complete: function (result) {
             if (result.status == 204) {
-                console.log("No content")
                 $(row).remove();
-            }
-            else if (result.status == 201) {
-                table.prepend(result.responseText);
             }
             else {
                 alert("Something went wrong pls try again");
